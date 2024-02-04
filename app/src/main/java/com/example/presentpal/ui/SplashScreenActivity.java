99ba78cb52c042de02cc4be2ff92afd94af64977
Alex Mihel <alex.mihel@hs-osnarueck.de>
2024-02-04 23:13:13 +0100

    splashscreen eingestellt

--- app/src/main/java/com/example/presentpal/ui/SplashScreenActivity.java
+++ app/src/main/java/com/example/presentpal/ui/SplashScreenActivity.java
@@ -2,8 +2,11 @@ package com.example.presentpal.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
+import com.example.presentpal.MainActivity;
 import com.example.presentpal.R;
 
 public class SplashScreenActivity extends AppCompatActivity {
@@ -12,5 +15,13 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
     }
 }
